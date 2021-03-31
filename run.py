13ea1e7d26680e0d6bb542da489b5cc8d03f6231
yangyang <youngqiankun@163.com>
2021-03-31 20:21:11 +0800

    update

--- run.py
+++ run.py
@@ -93,6 +93,6 @@ if __name__ == '__main__':
 if __name__ != '__main__':
     import logging
 
-    gunicorn_logger = logging.getLogger('gunicorn.info')
+    gunicorn_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
