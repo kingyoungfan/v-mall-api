3946b7f478d77a791ea9e5def30d160098d5d229
yangyang <youngqiankun@163.com>
2021-03-31 16:36:52 +0800

    update

--- logs/gun.py
+++ logs/gun.py
@@ -3,3 +3,4 @@ loglevel = 'info'
 accesslog = '/data/logs/jipin/access.log'  # gunicorn 访问日志
 errorlog = '/data/logs/jipin/error.log'  # 错误信息日志
 access_log_format = '%(h) -  %(t)s - %(u)s - %(s)s %(H)s'
+disable_existing_loggers = False
