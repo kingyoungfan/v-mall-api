73417187fb6650671192cc4cd7537bc9160b27e3
yangyang <youngqiankun@163.com>
2021-03-31 20:51:30 +0800

    update

--- ExtendRegister/excep_register.py
+++ ExtendRegister/excep_register.py
@@ -52,8 +52,8 @@ def tb(excep):
 
 @route_api.app_errorhandler(Exception)
 def errors(e):
-    print('异常:', e)
-    print('异常类型:', type(e))
+    current_app.logger.info('异常:', e)
+    current_app.logger.info('异常类型:', type(e))
 
     if isinstance(e, CustomException):
         current_app.logger.info('-----CustomException-----')
