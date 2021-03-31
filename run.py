d3918685803531830c31358efbce5105a9f87b77
yangyang <youngqiankun@163.com>
2021-03-31 14:13:56 +0800

    update

--- run.py
+++ run.py
@@ -60,6 +60,10 @@ def main():
 
 
 if __name__ == '__main__':
+    import logging
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
     pass
     """
     # 设置环境
