426ba70926ff854455d9bde773439ccc84c9a1f9
yangyang <youngqiankun@163.com>
2021-03-31 14:28:05 +0800

    update

--- logs/gun.py
+++ logs/gun.py
@@ -1,4 +1,5 @@
-
 loglevel = 'debug'
-logfile = '/data/logs/jipin/debug.log' #debug日志
-errorlog = '/data/logs/jipin/error.log' #错误信息日志
+# logfile = '/data/logs/jipin/debug.log' #debug日志
+accesslog = '/data/logs/jipin/access.log'  # gunicorn 访问日志
+errorlog = '/data/logs/jipin/error.log'  # 错误信息日志
+access_log_format = '%(h) -  %(t)s - %(u)s - %(s)s %(H)s'
