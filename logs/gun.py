e8b5e44286569c513edf3170475cfe60021d3a95
yangyang <youngqiankun@163.com>
2021-03-31 20:37:57 +0800

    update

--- logs/gun.py
+++ logs/gun.py
@@ -6,6 +6,6 @@ loglevel = 'info'
 accesslog = '-'  # gunicorn 访问日志
 errorlog = '-'  # 错误信息日志
 # access_log_format = '%(h) -  %(t)s - %(u)s - %(s)s %(H)s'
-capture_output = True
 # 代码更新重启
+logger_class = 'log_config.CustomLogger'
 reload = True
