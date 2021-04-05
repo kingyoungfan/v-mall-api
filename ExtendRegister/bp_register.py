2c52c0376c9cd3df33e48106939a014c947da302
yangyang <youngqiankun@163.com>
2021-04-05 15:22:58 +0800

    update

--- ExtendRegister/bp_register.py
+++ ExtendRegister/bp_register.py
@@ -7,6 +7,7 @@
 
 
 from app.api import route_api
+from app.api.mobile.user import mobile_bp
 from app.controllers.cms.cms_bp import route_admin
 from app.controllers.other_module_01.module_01 import route_module_01
 from app.controllers.other_module_02.module_02 import route_module_02
@@ -26,3 +27,6 @@ def register_bp(app):
     app.register_blueprint(route_module_01, url_prefix="/m1")
     app.register_blueprint(route_module_02, url_prefix="/m2")
     app.register_blueprint(route_module_03, url_prefix="/m3")
+
+    # 移动端API
+    app.register_blueprint(mobile_bp, url_prefix='/api/m')
--- app/api/mobile/user.py
+++ app/api/mobile/user.py
@@ -1,3 +1,11 @@
 # -*- coding: utf-8 -*-
 # @author: yangyang
 # @date 4/2/21 16:51
+from flask import Blueprint
+
+mobile_bp = Blueprint('mobile', __name__)
+
+
+@mobile_bp.route('/index')
+def index():
+    return {'code': 0, 'msg': 'mobile index'}
