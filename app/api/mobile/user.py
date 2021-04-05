2b47095c05b84e79d7fde7f4c4b114f68ec599ee
yangyang <youngqiankun@163.com>
2021-04-05 15:31:09 +0800

    update

--- app/api/mobile/user.py
+++ app/api/mobile/user.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 # @author: yangyang
 # @date 4/2/21 16:51
-from flask import Blueprint
+from flask import Blueprint, request, current_app
 
 from app.api.mobile import mobile_bp
 
 
 @mobile_bp.route('/index')
 def index():
+    token = request.headers.get("token")
+    current_app.logger.info('===>token: ' + token)
+
     return {'code': 0, 'msg': 'mobile index'}
